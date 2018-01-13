ac7b3d9d45b32aaed95ab81b84a348f7dcc0d4e7
Cassioy <cassioyoshi@hotmail.com>
2018-01-13 21:20:31 -0200

    initial commit

---
+++ app/src/main/java/br/cassioy/bakingapp/RecipeMainActivity.java
@@ -0,0 +1,14 @@
+package br.cassioy.bakingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecipeMainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_main);
+    }
+
+}
